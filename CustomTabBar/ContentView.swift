6242c3e997a4c7542c007790c6eee730478800b7
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-19 15:33:16 -0400

    adds views to each tab

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
 
     @State var selectedIndex = 0
+    @State var shouldShowModal = false
     
     let tabBarImageNames = ["person", "gear", "plus.app.fill", "pencil", "lasso"]
 
@@ -17,27 +18,58 @@ struct ContentView: View {
         VStack {
 
             ZStack {
+                
+                Spacer().fullScreenCover(isPresented: $shouldShowModal, content: {
+                    Button(action: {shouldShowModal.toggle()}, label: {
+                        Text("Fullscreen Cover")
+                    })
+
+                })
 
                 switch selectedIndex {
                 case 0:
                     NavigationView {
-                        Text("First")
-                            .navigationTitle("First Tab")
+                        ScrollView {
+                            ForEach(0..<100) { num in
+                                Text("\(num)")
+                            }
+                        }.navigationTitle("First screen")
+                    }
+                case 1:
+                    ScrollView {
+                        Text("Test")
                     }
                 default:
-                    Text("Remaining tabs")
+                    NavigationView {
+                        Text("Remaining tabs")
+                    }
                 }
 
             }
 
-            Spacer()
+            
+            Divider()
+            
             HStack {
                 ForEach(0..<5) { num in
-                    Button(action: {}, label: {
+                    Button(action: {
+                        if num == 2 {
+                            shouldShowModal.toggle()
+                            return
+                        }
+                    }, label: {
                         Spacer()
-                        Image(systemName: tabBarImageNames[num])
-                            .font(.system(size: 24, weight: .bold))
-                            .foregroundColor(Color(.label))
+                        
+                        if num == 2 {
+                            Image(systemName: tabBarImageNames[num])
+                                .font(.system(size: 44, weight: .bold))
+                                .foregroundColor(.red)
+                        } else {
+                            Image(systemName: tabBarImageNames[num])
+                                .font(.system(size: 24, weight: .bold))
+                                .foregroundColor(selectedIndex == num ? .black : .init(white: 0.8))
+                        }
+
                         Spacer()
                     })
 
